c8e42f6e853b868d5ad38cdb57fb8f7b41b9816d
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-10 19:30:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+fruit = input("What is food?")
+
+while fruit == "apple":
+  print ("True")
+  fruit = input("What is food?")
+if fruit == "orange":
+  print ("Orange you glad I didn't say banana?")
+if fruit == "banana":
+  print ("There I said it.")
